f0777705030c607966a5c04ca0a2406c40ca42b7
bmranganayakulu123 <bmranganayakulu@gmail.com>
2025-05-13 19:20:46 +0530

    Create basiccode.js

---
+++ basiccode.js
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Simple Web Page</title>
+</head>
+<body>
+  <h1>Hello, World!</h1>
+  <p>This is a basic HTML page served with Node.js.</p>
+</body>
+</html>
